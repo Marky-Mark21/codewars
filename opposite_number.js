424b2fcc534dfd9291d940300a70a1f4ede65e9f
Mark Apolinar <mark.apolinar@gmail.com>
2020-07-11 12:33:16 -0700

    opposite number.js

---
+++ opposite_number.js
@@ -0,0 +1,10 @@
+// codewar link: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+function opposite(number) {
+  if (number > 0) {
+    return - + number;
+  } else if (number < 0) {
+    return Math.abs(number);
+  }
+  return 0;
+};
