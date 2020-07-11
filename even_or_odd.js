d22a5c8a741a0f5b95c35efb6b84dad515b6bf04
Mark Apolinar <mark.apolinar@gmail.com>
2020-07-11 11:53:05 -0700

    solved even or odd

---
+++ even_or_odd.js
@@ -0,0 +1,10 @@
+// challenge url: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function even_or_odd(number) {
+  if (number % 2 !== 0) {
+    return 'Odd';
+  } else {
+    return 'Even';
+  }
+};
+
